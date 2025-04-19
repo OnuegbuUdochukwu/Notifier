d60209e60e01df8c41ab44776b9690c99ffbb1ee
mba emmanuel <Emmanuelmba329@gmail.com>
2025-04-19 20:08:07 +0100

    sign in form

--- notifier-frontend/src/route.jsx
+++ notifier-frontend/src/route.jsx
@@ -1,5 +1,6 @@
 import { lazy, Suspense } from "react";
 import { createBrowserRouter } from "react-router-dom";
+import SignUp from "./pages/SignUp";
 
 
 const Home = lazy(() => import('./pages/Home'));
@@ -9,7 +10,7 @@ const router = createBrowserRouter([
     path: '/',
     element: (
       <Suspense fallback={<div>Loading...</div>}>
-        <Home />
+        <SignUp/>
       </Suspense>
     )
   }
