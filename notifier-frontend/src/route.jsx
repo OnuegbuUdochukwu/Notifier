ec6bc6df38a3359a4c89b2031e90cec7e7115582
Emmanuel_A37 <akanjiemmanuel37@gmail.com>
2025-04-21 12:07:45 +0100

    updated route file

--- notifier-frontend/src/route.jsx
+++ notifier-frontend/src/route.jsx
@@ -1,19 +1,19 @@
 import { lazy, Suspense } from "react";
 import { createBrowserRouter } from "react-router-dom";
-import SignUp from "./pages/SignUp";
 
 
-const Home = lazy(() => import('./pages/Home'));
+const SignUp = lazy(() => import('./pages/SignUp'));
+
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: (
-      <Suspense fallback={<div>Loading...</div>}>
-        <SignUp/>
-      </Suspense>
-    )
-  }
+    {
+        path : '/',
+        element : (
+            <Suspense>
+                <SignUp/>
+            </Suspense>
+        )
+    }
 ]);
 
-export default router;
+export default router
