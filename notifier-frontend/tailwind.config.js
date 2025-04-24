3f268ffc6b99f9823ac31ae5a55883f08840bb50
Akinwaleogundipe <akinwaleogundipe245@gmail.com>
2025-04-24 21:56:13 +0100

    Landing Page

--- notifier-frontend/tailwind.config.js
+++ notifier-frontend/tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customblue: "rgba(9, 69, 93, 1)",
+      },
+    },
   },
   plugins: [],
-}
+};
 
 
